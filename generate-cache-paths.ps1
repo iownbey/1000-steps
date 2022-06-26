4b7cfce603466e0c503a32e6091d9668afa64dda
iownbey <54186227+iownbey@users.noreply.github.com>
2022-06-25 23:33:49 -0400

    Updated Cache Manifest

--- generate-cache-paths.ps1
+++ generate-cache-paths.ps1
@@ -1,1 +1,1 @@
-Get-ChildItem -File -Recurse | Where-Object {$_.FullName -CNotMatch "\.github|\.ps1|\.bat"} | Resolve-Path -Relative | ForEach-Object {"'${_}',"} | ForEach-Object {$_.Replace("\","/")}
+Get-ChildItem -File -Recurse | Where-Object {$_.FullName -CNotMatch "\.github|\.ps1|\.bat|\.flp|\\Old\\"} | Resolve-Path -Relative | ForEach-Object {"'${_}',"} | ForEach-Object {$_.Replace("\","/")}
