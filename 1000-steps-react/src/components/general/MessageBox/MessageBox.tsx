ff1c2258bbcbf28aa0a5f08d08a5779c5e5f704b
iownbey <54186227+iownbey@users.noreply.github.com>
2024-09-17 22:28:08 -0700

    messsage box decorations

--- 1000-steps-react/src/components/general/MessageBox/MessageBox.tsx
+++ 1000-steps-react/src/components/general/MessageBox/MessageBox.tsx
@@ -11,6 +11,36 @@ export const MessageBox = ({
 }: React.PropsWithChildren<MessageBoxType>) => {
   return (
     <div className="message-box" style={style}>
+      <div
+        className="decoration"
+        style={{
+          top: "0px",
+          left: "0px",
+          filter: "brightness(0.7)",
+        }}
+      />
+      <div
+        className="decoration"
+        style={{
+          top: "0px",
+          right: "0px",
+          transform: "scaleX(-1)",
+          filter: "brightness(0.85)",
+        }}
+      />
+      <div
+        className="decoration"
+        style={{ bottom: "0px", right: "0px", transform: "scale(-1)" }}
+      />
+      <div
+        className="decoration"
+        style={{
+          bottom: "0px",
+          left: "0px",
+          transform: "scaleY(-1)",
+          filter: "brightness(0.85)",
+        }}
+      />
       {children}
     </div>
   );
